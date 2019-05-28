9c42830135492cb29e0e7bee07811f20d8048205
vinu1499 <47626255+vinu1499@users.noreply.github.com>
2019-05-28 17:17:05 +0530

    Update hackerrank.py

--- hackerrank.py
+++ hackerrank.py
@@ -3,3 +3,15 @@ number=list(map(int,input().split()))
 print(0,end=" ")
 for i in range(1,len(number)):
     print(number[i-1]*2,end=" ")
+# infy 01
+def add(str1):
+  if(len(str1)>=3):
+        if(str1.endswith("ing")):
+            str1=str1+"ly"
+        else:
+            str1=str1+"ing"
+  
+  return str1
+
+str1="sleep"
+print(add(str1))
